a905bd25f16ee3877e579294ecebdd426f442c71
AegeanKI <pmes.986612@gmail.com>
2023-06-28 17:19:24 +0800

    fix prioritize_random_sample

--- utils.py
+++ utils.py
@@ -86,9 +86,9 @@ class Memory():
             #  (state, condition, action_idx, reward, done, next_state, next_condition)
             weights = torch.tensor(self.buffers[3]).view(-1).abs() + 1 / self.maxlen
             idx = torch.multinomial(weights[n_frames - 1:], 1)[0] + n_frames - 1 # idx is last
-            count_done = self.buffers[4][idx + 1 - n_frame:idx + 1 - 1].sum() # last frame can be done
+            count_done = self.buffers[4][idx + 1 - n_frames:idx + 1 - 1].sum() # last frame can be done
 
-        return self[idx + 1 - n_frame:idx + 1]
+        return self[idx + 1 - n_frames:idx + 1]
 
 def unpackbits(x, num_bits):
     if np.issubdtype(x.dtype, np.floating):
